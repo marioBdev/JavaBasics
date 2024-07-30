5ca85e0b1fb40e60690511d36217437c80a2a3c4
marioBdev <bacana.vip@gmail.com>
2024-07-30 05:28:58 +0100

    Update TimeStamp.java
    Add main class to execute this code

--- TimeStamp.java
+++ TimeStamp.java
@@ -1,5 +1,13 @@
-    public String currentDateTime (){
+public class Datatime{
+
+public String currentDateTime (){
         long timestamp1 = System.currentTimeMillis() /1000;
         LocalDateTime datatime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp1));
         return date;
     }
+    String hora = currentDateTime().toString();
+}
+  public static void main(String[] args) {
+      Datatime timeNow = new Datatime();
+      System.out.print(timeNow);
+  }
